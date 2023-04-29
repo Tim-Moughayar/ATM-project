f1857a6866e897c5d86a644d1e2b520d42a60553
Tim-Moughayar <121068801+Tim-Moughayar@users.noreply.github.com>
2023-04-28 19:16:56 -0500

    Update and rename customer_console_assignment_14.py to source_code/customer_console.py

--- source_code/customer_console.py
+++ source_code/customer_console.py
@@ -1,46 +1,26 @@
 """Customer console class for ATM.
-
 Input:
     Customer Pin
     Customer money amount
     Customer menu choice
-
 Output:
     Display message to customer
-
 References: 
     * https://www.math-cs.gordon.edu/courses/cs211/ATMExample/javadoc/atm/physical/CustomerConsole.html
-
 Created by Frank Boxenbaum
 """
-
 import getpass
 
 
 class CustomerConsole():
     """Customer console class for ATM."""
     def __init__(self):
-        """Creates a customer console
-
-        Args:
-            read_pin (integer): Customer pin number
-            read_menu_choice (integer): Customer menu choice
-
-        Returns:
-            read_amount (float): Customer entered money amount
-        """
-
-        self._read_pin = None
-        self._read_menu_choice = None
-        self._read_amount = None
-        self._display = None
+        """Costumer console constructor"""
 
     def read_pin(self):
         """Gets user's pin number
-
         Returns:
             integer: Customer pin number
-
         """
         print('Please insert your card')
         pin = getpass.getpass("Please enter your pin:")
@@ -50,10 +30,8 @@ class CustomerConsole():
 
     def read_amount(self):
         """Gets user's dollar amount
-
         Returns:
             float: User's desired dollar amount
-
         """
         amount = input("Please enter the desired $ amount.")
         amount = float(amount)
@@ -62,9 +40,7 @@ class CustomerConsole():
 
     def display(self):
         """Displays the menu of ATM options.
-
         Returns: None
-
         """
 
         print('Please select an option')
@@ -74,13 +50,11 @@ class CustomerConsole():
 
     def read_menu_choices(self):
         """Gets user's menu choice.
-
         Returns:
             integer: User's desired menu choice
-
         """
         choice = input()
         choice = int(choice)
-        if choice
+        # if choice
 
         return choice
