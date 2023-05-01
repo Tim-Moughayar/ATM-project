dcc10f402c8e5d7e600e2d587cb175a4c4c43546
Tim-Moughayar <121068801+Tim-Moughayar@users.noreply.github.com>
2023-04-30 21:13:38 -0500

    this also works now

--- source_code/transaction.py
+++ source_code/transaction.py
@@ -1,13 +1,4 @@
-"""Transaction class for ATM. Starts a Withdrawal, Deposit or Inquiry transaction.
-Input:
-
-Output:
-    
-References: 
-
-"""
-from atm import Atm
-
+"""Transaction class for ATM. Starts a Withdrawal, Deposit or Inquiry transaction."""
 
 class Transaction():
     """Starts a Withdrawal, Deposit or Inquiry transaction."""
@@ -22,8 +13,23 @@ class Transaction():
         """
         self.atm = atm
         self.card = card
+        self.pin = pin
         self.serial_number = serial_number + 1
         # self.balance = Balances()
 
     def perform_transaction(self):
-        Atm.get_customer_console(self).display
+        """Asks user if they want to Withdraw, Deposit 
+        or check balance and executes it.
+
+        Returns: None
+        """
+        prompt = 'Please select an option'
+        options = ['Account Balance', 'Withdraw', 'Deposit']
+        choice = self.atm.get_customer_console().read_menu_choices(self, prompt, options)
+
+        if choice == 1:
+            print("Getting account balance.") # TEMPORARY
+        elif choice == 2:
+            print("Starting withdrawal.") # TEMPORARY
+        elif choice == 3:
+            print("Starting deposit.") # TEMPORARY
