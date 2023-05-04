efeecb6684f206fb175f37632d521607759a741f
Tim-Moughayar <121068801+Tim-Moughayar@users.noreply.github.com>
2023-05-03 21:52:55 -0500

    not completely done with changes

--- source_code/atm.py
+++ source_code/atm.py
@@ -30,11 +30,13 @@ class Atm:
         # CashDispenser.set_initial_cash(cash_in_atm)
 
         # gets pin from user
-        pin = CustomerConsole.read_pin(self)
-        card = 5425233430109903
+        while True:
+            pin = CustomerConsole.read_pin(self)
+            card = 5425233430109903
 
-        transaction = Transaction(self, card, pin, 1)
-        Transaction.perform_transaction(transaction)
+            transaction = Transaction(self, card, pin, 1)
+            Transaction.perform_transaction(transaction)
+            Transaction.repeat_transaction(transaction)
 
     def get_ID(self):
         return self.id
