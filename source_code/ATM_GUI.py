c71eff18a35372851b390d802144d20983fdb49e
CptMalReynolds <106292445+CptMalReynolds@users.noreply.github.com>
2023-05-04 15:23:45 -0500

    Added functionality to GUI
    Made output text box read-only for user.
    Clear button now clears text from the User Input field.
    Added function to update output text box.
    Enter function now retrieves user input from the User Input field and shows it in the output text box. Function also clears User Input field after input is retrieved.
    Added function to retrieve value from User Input field.

--- source_code/ATM_GUI.py
+++ source_code/ATM_GUI.py
@@ -8,12 +8,35 @@ class Window(Frame):
 root = Tk()
 app = Window(root)
 
-output_label = Label(text="ATM Output:")
-output_box = Text(root, height=15, width=30, bg="light yellow")
-input_label = Label(text="User Input:")
-input_box = Entry(root, width=30, textvariable=None, bg="light yellow")
-clear_button = Button(root, text="Clear", command=lambda: None)
-enter_button = Button(root, text="Enter", command=lambda: None)
+def clear_input_box():
+    input_box.delete(0,END)
+
+def update_output_box(update_text):
+    output_box.configure(state=NORMAL) # unlocks output_box so it can be updated
+    output_box.insert(END, update_text) # inserts update_text at the end of the text in output_box
+    output_box.insert(END, "\n") # adds a line break after the inserted text
+    output_box.configure(state=DISABLED) # locks output_box so it can no longer be updated
+    output_box.yview_moveto(1) # scrolls to the bottom of output_box
+
+
+def get_input_box():
+    user_input = input_box.get()
+    return user_input
+
+def submit_user_input():
+    user_input = get_input_box()
+    update_output_box(user_input)
+    historical_input.append(user_input)
+    clear_input_box()
+
+historical_input = []    
+
+output_label = Label(text="ATM Output:") # label for output_box
+output_box = Text(root, height=15, width=30, bg="light yellow", state=DISABLED) # creates read-only text box for program output
+input_label = Label(text="User Input:") # label for input_box
+input_box = Entry(root, width=30, textvariable=None, bg="light yellow") # entry widget for user input
+clear_button = Button(root, text="Clear", command=lambda: clear_input_box()) # button to clear text from input_box
+enter_button = Button(root, text="Enter", command=lambda: submit_user_input()) # button to submit user input
 
 
 root.wm_title("Welcome to Ponzi Bank")
