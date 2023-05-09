fb3debf472632a505eb2f342a9db4399c55d7dc7
ravinam95 <84754757+ravinam95@users.noreply.github.com>
2023-05-09 13:28:07 -0500

    Update main_balances.py

--- source_code/main_balances.py
+++ source_code/main_balances.py
@@ -1,17 +1,9 @@
 from customer_console import CustomerConsole
 from balances import Balance
+from transaction import Transaction
 
 def main():
-    my_console = CustomerConsole()
-    my_choice_list = ['Deposit', 'Withdraw', 'Balance']
-    my_console.read_menu_choices('Choice: ', my_choice_list)
-    #if input() == '3':
-    pin = my_console.read_pin()
-    my_balance = Balance()
-    my_balance.pin = pin
-    account_balance = my_balance.balance()
-    print("Balance: $" + str(account_balance))
-
-    
+    my_transaction = Transaction("xyz","abc",0,45)
+    my_transaction.perform_transaction()
 
 main()
